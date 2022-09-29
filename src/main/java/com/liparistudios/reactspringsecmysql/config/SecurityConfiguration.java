f8396986fcab8af48c531c500149e8215cdff7cc
LipariStudios <vitolipari81@gmail.com>
2022-09-29 07:37:57 +0200

    #wip

---
+++ src/main/java/com/liparistudios/reactspringsecmysql/config/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.liparistudios.reactspringsecmysql.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
