322f90a97c977f588c91c958989a004b9ed47e59
Vito Lipari <liparistudios@gmail.com>
2022-09-29 13:34:09 +0200

    #wip

---
+++ README.md
@@ -0,0 +1,29 @@
+seguendo qesta guida
+https://www.youtube.com/watch?v=KYNR5js2cXE
+
+sono giunto a 21:54
+
+
+a seguire
+https://www.danvega.dev/blog/2022/05/12/spring-data-jpa-pagination/
+
+
+
+per la configurazione jwt
+creazione di una coppia di chiavi
+
+generazione delle chiavi
+```bash
+openssl genrsa -out ./keypair.pem 2048
+```
+
+estrazione della chiave pubblica
+```bash
+openssl rsa -in ./keypair.pem -pubout -out ./public.pem
+```
+
+estrazione della chiave privata
+```bash
+openssl pkc8 -topk8 -inform PEM -outform PEM -nocrypt -in ./keypair.pem -out ./private.pem
+```
+
--- src/main/java/com/liparistudios/reactspringsecmysql/config/SecurityConfiguration.java
+++ src/main/java/com/liparistudios/reactspringsecmysql/config/SecurityConfiguration.java
@@ -1,11 +1,91 @@
 package com.liparistudios.reactspringsecmysql.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity( prePostEnabled = true )
+public class SecurityConfiguration {
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return (
+            http
+
+                // CORS
+                .cors()
+
+                // CSRF disabilitato
+                .and()
+                .csrf( csfr -> csfr.disable() )
+                // .csrf().disable()
+
+                /*
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse()))
+                    By default the CookieServerCsrfTokenRepository will write to a cookie named XSRF-TOKEN and read it from a header named X-XSRF-TOKEN or the HTTP parameter _csrf.
+                    These defaults come from AngularJS
+                    The sample explicitly sets cookieHttpOnly=false. This is necessary to allow JavaScript (i.e. AngularJS) to read it.
+                    If you do not need the ability to read the cookie with JavaScript directly, it is recommended to omit cookieHttpOnly=false
+                    (by using new CookieServerCsrfTokenRepository() instead) to improve security.
+                 */
+
+
+                .authorizeRequests( auth ->
+                    auth
+
+                        // rotte libere
+                        .antMatchers("").permitAll()
+                        .antMatchers("").permitAll()
+
+                        // rotte private
+                        .anyRequest().authenticated()
+
+                )
+
+
+                // Gestione eccezioni
+                .exceptionHandling()
+                .authenticationEntryPoint( unauthorizedHandler )
+
+                // Sessione
+                .and()
+
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+
+
+                .sessionManagement( session -> session.sessionCreationPolicy( SessionCreationPolicy.STATELESS ) )
+                // .sessionCreationPolicy( SessionCreationPolicy.STATELESS )
+
+
+
+
+
+
+
+                .and()
+                .httpBasic( Customizer.withDefaults() )
+                .build()
+
+        );
+        ;
+
+
+
+
+    }
+
+
 }
